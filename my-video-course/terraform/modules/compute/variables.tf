1829f2335584aef305b47c4b990c5a7cfed65419
Felex Kuria <engineerfelex@gmail.com>
2026-03-21 22:09:57 +0300

    feat: add image_tag variable

--- my-video-course/terraform/modules/compute/variables.tf
+++ my-video-course/terraform/modules/compute/variables.tf
@@ -5,7 +5,10 @@ variable "app_name" {
 variable "create_ecr_repo" {
   type = bool
 }
-
+variable "image_tag" {
+  description = "The Docker image tag to deploy"
+  type        = string
+}
 variable "create_asg" {
   type = bool
 }
